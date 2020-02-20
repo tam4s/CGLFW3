c357e6cc9b5f5b5fdf5a8e4f9ca270a1baead289
tam4s <2077099+tam4s@users.noreply.github.com>
2020-02-20 10:48:20 +0100

    Upgrade Package.swift descriptor

--- Package.swift
+++ Package.swift
@@ -1,5 +1,11 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "CGLFW3"
+    name: "CGLFW3",
+    pkgConfig: "glfw3",
+    providers: [
+        .brew(["glfw"])
+    ]
 )
